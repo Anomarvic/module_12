fa249232f3e2e590e21e46cfc6c0dee23e8a2d36
Anomarvic <anomarvic@gmail.com>
2025-01-10 11:36:54 +0300

    home_work_test

---
+++ tests_12_1.py
@@ -0,0 +1,26 @@
+from runner import Runner
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner('Rick')
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner('Thomas')
+        for j in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("Joy")
+        runner2 = Runner("Newt")
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+if __name__ == '__main__':
+    unittest.main()
